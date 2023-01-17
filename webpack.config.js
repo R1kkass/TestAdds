74e457fd94354de22bdd9aefbfdf0c2bb7076262
Your Name <you@example.com>
2023-01-17 22:43:08 +0300

    add bundl

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    //webpack-dev-server settings
+    devServer: {
+        static: {
+            contentBase: path.join(__dirname, "dist")
+        },
+        port: 3001,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.bundle\.ts$/,
+                use: {
+                    loader: 'bundle-loader',
+                    options: {
+                        name: '[name]'
+                    }
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\[name].(svg|png|gif|jpg)$/,
+                exclude: /fonts/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(ttf|eot|woff|svg|woff2)$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(ttf|eot|woff|svg|woff2)$/,
+                loader: "file-loader"
+            },{
+                test:/\.(s*)css$/,
+                use: [
+                    'style-loader',
+                   'css-loader',
+                   'sass-loader',
+                ]
+             },
+             {
+                test: /\[name].(png|jpe?g|gif)$/i,
+                use: [
+                    'file-loader',
+                ]
+             },
+             {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: "babel-loader"
+                }
+              },
+
+        ],
+    },
+    /* Attempt to resolve these extensions in order.
+       If multiple files share the same name but have
+       different extensions, webpack will resolve the
+       one with the extension listed first in the
+       array and skip the rest.
+     */
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        })
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
